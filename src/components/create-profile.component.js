c6d7f8ab519bd47fcc1c35930b691a564cd6ed50
SayedSarwary <sarwary@pdx.edu>
2022-05-09 20:42:28 -0700

    changes done

---
+++ src/components/create-profile.component.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
+
+const CreateExercise = () => {
+  const [Exericse, setExericse] = useState({
+    username: "helllo",
+    description: "",
+    URLS: 0,
+    date: new Date(),
+  });
+
+  const [Users, setUsers] = useState([]);
+
+  const onSubmit = () => {
+    axios.post("http://localhost:5000/exercises/add", Exericse);
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/users").then((res) => {
+      res.data.map((user) => {
+        setUsers((oldArray) => [...oldArray, user.username]);
+        return 0;
+      });
+    });
+  }, []);
+
+  return (
+    <div>
+      <h3>Create New Profile Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            required
+            className="form-control"
+            value={Exericse.username}
+            onChange={(e) =>
+              setExericse({ ...Exericse, username: e.target.value })
+            }
+          >
+            {Users.map(function (user) {
+              return (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={Exericse.description}
+            onChange={(e) =>
+              setExericse({ ...Exericse, description: e.target.value })
+            }
+          />
+        </div>
+        <div className="form-group">
+          <label>URLS : </label>
+          <input
+            type="text"
+            className="form-control"
+            value={Exericse.URLS}
+            onChange={(e) => setExericse({ ...Exericse, URLS: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker
+              selected={Exericse.date}
+              onChange={(e) =>
+                setExericse({ ...Exericse, date: e.target.value })
+              }
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create Profile Log"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+export default CreateExercise;
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,26 +1,45 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default class Navbar extends Component {
-
-  render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
-        <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link">Exercises</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/user" className="nav-link">Create User</Link>
-          </li>
-        </ul>
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Profile Bar
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/create">
+                Create Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/user">
+                Create User
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
